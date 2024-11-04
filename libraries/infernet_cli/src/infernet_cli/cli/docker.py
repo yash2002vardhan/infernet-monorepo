429ce32e4bfeefb993883e985f8446e6143296e8
evangelos-ritual <evangelos@ritual.net>
2024-11-04 18:37:02 +0200

    If [{

--- libraries/infernet_cli/src/infernet_cli/cli/docker.py
+++ libraries/infernet_cli/src/infernet_cli/cli/docker.py
@@ -87,12 +87,12 @@ def health_check(dir: str) -> None:
         )
         # Parse the result as JSON to inspect each container
         containers = ",".join(result.stdout.split("\n")[:-1])
-        containers_obj = json.loads(containers)
-        
         # Tackle Mac/Linux compatibility of docker compose output   
-        if isinstance(containers_obj, dict): 
-            containers_obj = [containers_obj] 
-
+        if containers.startswith("[{"):
+            containers_obj = json.loads(containers)
+        else:
+            containers_obj = json.loads(f"[{containers}]") 
+        
         if len(containers_obj) == 0:
             click.echo("No containers found.")
             return
